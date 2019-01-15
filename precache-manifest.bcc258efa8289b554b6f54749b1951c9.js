46c9af8eb87cbbb3a41da34266cae64b15973b79
JaneGe <625806758@qq.com>
2019-01-16 00:22:26 +0800

    first releae

---
+++ precache-manifest.bcc258efa8289b554b6f54749b1951c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5f860fc5ad8c5f708d06",
+    "url": "/static/js/main.5f860fc5.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "5f860fc5ad8c5f708d06",
+    "url": "/static/css/main.3e4f2ac4.chunk.css"
+  },
+  {
+    "revision": "0f48d481f848fa2003801116c3756ce9",
+    "url": "/index.html"
+  }
+];
